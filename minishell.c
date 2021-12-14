89c7b516ffe66d12aa4dd9a93a5fa6027acca6bf
Tho TRINH <lam-cao-tho.trinh@etu.u-pec.fr>
2021-12-14 01:04:27 +0100

    Update minishell.c with int parse_line fonction works + pwd + exit work

--- minishell.c
+++ minishell.c
@@ -18,7 +18,7 @@
 
 
 /* Fonction prototype */
-int parseline(char *s, char **argv);
+int parse_line(char *s, char **argv);
 
 /* ----------- MAIN PROGRAM ------------ */
 
@@ -26,6 +26,7 @@ int main(void) {
     
     char  cmdline[MAXLINE];
     char *argv[MAXARGS];
+    int path[MAX_PATH_LENGTH];
     int argc;
     int status;
     pid_t pid;
@@ -33,6 +34,48 @@ int main(void) {
     /* Boucle while infinie pour attendre l'utilisateur rentre une commande -> executer commande*/
     while(1){
         /* Nom de cet mini shell */
-        printf("TM-minishell > ");
+        printf("TM-minishell > \n");
+        printf("$ ");
+
+        /* Lire entrer command line*/
+        fgets(cmdline, MAXLINE, stdin);
+
+        argc = parse_line(cmdline, argv);
+
+        if(argc == 0) {
+            continue;
+        }
+
+        if(strcmp(argv[0], "exit") == 0){
+            return EXIT_SUCCESS;
+        }
+
+        if(strcmp(argv[0], "pwd") == 0){
+            if (getcwd(path, MAX_PATH_LENGTH)){
+                printf("%s\n", path);
+            }else {
+                printf("Ne pas afficher le répertoire de travail courant.\n");
+            }
+            continue;
+        }
+
+
+
+
     }
+
+    
 }
+
+int parse_line(char *s, char **argv){
+        int count = 0;
+        char *spaceEnterTab = " \n\t";
+
+        argv[count] = strtok(s, spaceEnterTab);
+
+        while ((argv[count] != NULL) && (count < MAXARGS -1)){
+            argv[++count] = strtok((char *) 0, spaceEnterTab);
+        }
+
+        return count;
+    }
